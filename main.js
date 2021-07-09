816094640b5cf4ec084f1780362df6273acd3a46
dongxingyan <dongxingyan3@jd.com>
2021-07-09 11:30:12 +0800

    第一步:createElement

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-function main() {
-    console.log('e');
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child =>
+                typeof child === 'object'
+                    ? child
+                    : createTextElement(child)
+            )
+        }
+    };
 }
-main();
+
+function createTextElement(text) {
+    return {
+        type: 'TEXT_ELEMENT',
+        props: {
+            nodeValue: text,
+            children: []
+        }
+    };
+}
+
+const Didact = {
+    createElement
+};
+
+/** @jsx Didact.createElement */
+const element = (
+    <div id='foo'>
+        <h1>study react</h1>
+        <p>build your own react</p>
+    </div>
+);
+console.log(element);
